d954406d6fdddcd8773e3c114253645d6c2f3b03
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-09-30 09:56:30 -0300

    update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import style from './style.module.css';
 import data from '@/data/data.json';
+import {FaLinkedin, FaGithub, FaMedium, FaRedditAlien, FaInstagram, FaXTwitter} from 'react-icons/fa6';
 
 export function Footer(){
     return(
@@ -8,32 +9,38 @@ export function Footer(){
             <ul className={style.footerList}>
                 <li className={style.footerItem}>
                     <Link href={data.contact_user.linkedin} className={style.footerLink}>
-                        <span className={style.footerIcon}>🔗 LinkedIn</span>
+                        <FaLinkedin className={style.footerIcon} />
+                        <span className={style.footerText}>LinkedIn</span>
                     </Link>
                 </li>
                 <li className={style.footerItem}>
                     <Link href={data.contact_user.github} className={style.footerLink}>
-                        <span className={style.footerIcon}>🐱 GitHub</span>
+                        <FaGithub className={style.footerIcon} />
+                        <span className={style.footerText}>GitHub</span>
                     </Link>
                 </li>
                 <li className={style.footerItem}>
                     <Link href={data.contact_user.medium} className={style.footerLink}>
-                        <span className={style.footerIcon}>✍️ Medium</span>
+                        <FaMedium className={style.footerIcon} />
+                        <span className={style.footerText}>Medium</span>
                     </Link>
                 </li>
                 <li className={style.footerItem}>
                     <Link href={data.contact_user.reddit} className={style.footerLink}>
-                        <span className={style.footerIcon}>👾 Reddit</span>
+                        <FaRedditAlien className={style.footerIcon} />
+                        <span className={style.footerText}>Reddit</span>
                     </Link>
                 </li>
                 <li className={style.footerItem}>
                     <Link href={data.contact_user.instagram} className={style.footerLink}>
-                        <span className={style.footerIcon}>📸 Instagram</span>
+                        <FaInstagram className={style.footerIcon}/>
+                        <span className={style.footerText}>Instagram</span>
                     </Link>
                 </li>
                 <li className={style.footerItem}>
                     <Link href={data.contact_user.x} className={style.footerLink}>
-                        <span className={style.footerIcon}>🐦 X</span>
+                        <FaXTwitter className={style.footerIcon} />
+                        <span className={style.footerText}>X</span>
                     </Link>
                 </li>
             </ul>
